4cfc35a3354c7c78f8f6e6a3d87bd712f1ecc516
roy <nyarkoyeboahroy@gmail.com>
2025-01-28 10:27:23 +0000

    Initial Commit

---
+++ src/test/java/org/roy/kanco/KanCoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roy.kanco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanCoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
